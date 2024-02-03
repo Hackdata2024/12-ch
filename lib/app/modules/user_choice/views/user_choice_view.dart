4b715fb68cf8c8325302d6bae4a48f4435e63831
Deepakkdutt <bhagatdeepak1242@gmail.com>
2024-02-03 18:33:33 +0530

    deepak 2

--- lib/app/modules/user_choice/views/user_choice_view.dart
+++ lib/app/modules/user_choice/views/user_choice_view.dart
@@ -10,40 +10,45 @@ class UserChoiceView extends GetView<UserChoiceController> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color(0xfff0F5697),
+        backgroundColor: Color(0xfffC7D9EA),
         body: Column(
       
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 12,),
-            Text("Welcome to",style: TextStyle(color: Colors.white,fontSize: 20),),
+            SizedBox(height: 20,),
+            Text("Welcome to",style: TextStyle(color: Color(0xfff0F5697),fontSize: 20,fontWeight: FontWeight.w500),),
             Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Acad",style: TextStyle(color: Color.fromARGB(255, 87, 135, 207),fontSize: 35),),
-              Text("ease",style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),fontSize: 35),),
+              Text("Acad",style: TextStyle(color: Color(0xfff0F5697),fontSize: 35,fontWeight: FontWeight.w800),),
+              Text("ease",style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),fontSize: 35,fontWeight: FontWeight.w800),),
 
             ],
 
           ),
+          SizedBox(height: 40,),
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Container(
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 25),
-                child: Column(
-                  children: [
-                    Image.asset('assests/images/Teacher.png',fit: BoxFit.cover,),
-                    SizedBox(height: 5,),
-                    Text('Faculty',style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
-                  ],
+                child: InkWell(
+                
+                  onTap: (){},
+                  child: Column(
+                    children: [
+                      Image.asset('assets/Teacher.png',fit: BoxFit.cover,),
+                      SizedBox(height: 5,),
+                      Text('Faculty',style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
+                    ],
+                  ),
                 ),
               ),
               height: 250,
               width:MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                color: Colors.blueAccent.shade200,
+                color: Color(0xfff0F5697),
                 borderRadius: BorderRadius.circular(15)
               ),
             ),
@@ -54,18 +59,23 @@ class UserChoiceView extends GetView<UserChoiceController> {
             child: Container(
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 25),
-                child: Column(
-                  children: [
-                    Image.asset('assests/images/Student Male.png',fit: BoxFit.cover,),
-                    SizedBox(height: 5,),
-                    Text('Student',style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
-                  ],
+                child: InkWell(
+                  onTap: () {
+                    
+                  },
+                  child: Column(
+                    children: [
+                      Image.asset('assets/Student Male.png',fit: BoxFit.cover,),
+                      SizedBox(height: 5,),
+                      Text('Student',style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
+                    ],
+                  ),
                 ),
               ),
               height: 250,
               width:MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                color: Colors.blueAccent.shade200,
+                color: Color(0xfff0F5697),
                 borderRadius: BorderRadius.circular(15)
               ),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:acadease/app/modules/attendance/views/attendance_view.dart';
 import 'package:acadease/app/modules/attendance/views/choose_class.dart';
 import 'package:acadease/app/modules/login/views/login_view.dart';
+import 'package:acadease/app/modules/user_choice/views/user_choice_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -13,7 +14,7 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: "Application",
       // initialRoute: AppPages.INITIAL,
-      home: ChooseClass(),
+      home: UserChoiceView(),
       getPages: AppPages.routes,
       // routes: {
       //   'UserChoiceView' : (context) => UserChoiceView(),
